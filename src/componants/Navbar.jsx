d9a5532360ef5dde5a8b103d03621caee1d57308
test <test@example.com>
2024-09-26 16:27:35 +0300

    up

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -36,15 +36,15 @@ export default function Navbar() {
                 <div>
                 <img src="https://early-wildflower-281.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffb56a855-0c74-4a0d-84dc-3fb7c76ae0b3%2Fe1e4f38a-a958-4b5d-bcb0-eddce8b433c0%2FUntitled.png?table=block&id=fff185a2-dff1-818a-9e69-c9f78f78f9f7&spaceId=fb56a855-0c74-4a0d-84dc-3fb7c76ae0b3&width=120&userId=&cache=v2" alt="" />
                 </div>
-                <h5>Benefit 1</h5>
-                <p>For example, restaurants and bakeries could outline the health benefits of their all- natural ingredients.</p>
+                <h5>Benefit 2</h5>
+                <p>Florists and other small retailers. might use this space to describe how their products make delightful, one-of-a-kind gifts.</p>
             </div>
             <div className='text-center flex flex-col place-items-center'>
                 <div>
                     <img src="https://early-wildflower-281.notion.site/image/https%3A%2F%2Fprod-files-secure.s3.us-west-2.amazonaws.com%2Ffb56a855-0c74-4a0d-84dc-3fb7c76ae0b3%2Fe1e4f38a-a958-4b5d-bcb0-eddce8b433c0%2FUntitled.png?table=block&id=fff185a2-dff1-818a-9e69-c9f78f78f9f7&spaceId=fb56a855-0c74-4a0d-84dc-3fb7c76ae0b3&width=120&userId=&cache=v2" alt="" />
                 </div>
-                <h5>Benefit 1</h5>
-                <p>For example, restaurants and bakeries could outline the health benefits of their all- natural ingredients.</p>
+                <h5>Benefit 3</h5>
+                <p>You could also add buttons to this section and share links to other products or available delivery methods.</p>
             </div>
         </div>
 
